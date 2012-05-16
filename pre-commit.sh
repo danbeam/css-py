7a2737f75f42aa172612a91d3671457adcd5c037
Dan Beam <dan@danbeam.org>
2012-05-16 01:44:13 -0700

    fixing snazziness

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,7 +2,7 @@
 
 echo "Running pre-commit tests...";
 for f in `/bin/ls ./css/*_unittest.py`; do
-  output=$(python $f);
+  output=$(python $f 2>&1);
   if [ $? -ne 0 ]; then
     echo "$output" >&2;
     exit 1
