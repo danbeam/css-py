7c17a364680ab3159d7b1701d01edf7b3c5cd843
Dan Beam <dan@danbeam.org>
2012-05-16 01:43:29 -0700

    making pre-commit hooks snazzier

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "Running pre-commit tests...";
 for f in `/bin/ls ./css/*_unittest.py`; do
-  python $f || exit $?
+  output=$(python $f);
+  if [ $? -ne 0 ]; then
+    echo "$output" >&2;
+    exit 1
+  fi
 done;
 
 exit 0
